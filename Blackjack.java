f3468be0a225080742040b0621d77663f7193069
Trevor Hodde <thodde@vivox.com>
2017-07-26 07:45:12 -0400

    win logic not quite right but its getting there

--- Blackjack.java
+++ Blackjack.java
@@ -6,13 +6,14 @@ import java.util.ArrayList;
 
 public class Blackjack {
 	static Scanner sc = new Scanner(System.in);
+	static Deck d;
 
 	public static void main(String[] args) {
 		DatabaseManager dm = new DatabaseManager();
 		char choice;
 
-		Deck deck = new Deck();
-		deck.shuffleDeck();
+		d = new Deck();
+		d.shuffleDeck();
 
 		System.out.print("Enter your full name: ");
 		String username = sc.nextLine();
@@ -29,58 +30,127 @@ public class Blackjack {
 
 		do {
 			//game play
-			play_game(deck, player);
+			play_game(player);
 
 			System.out.println("Do you want to play again? <y/n>");
 			choice = sc.next().charAt(0);
 		} while(choice != 'n');
 	}
 
-	public static void play_game(Deck d, User u) {
+	public static void play_game(User u) {
 		boolean playerTurn = true;
-		char choice;
+		int playerResult = 0;
+		int computerResult = 0;
+		char choice = 'n';
 		ArrayList<Card> playersHand = new ArrayList<Card>();
-		Card computersHand[];
+		ArrayList<Card> computersHand = new ArrayList<Card>();
 
-		do {
-			// starting the players turn
-			System.out.println("--------------------------");
+		if(playerTurn) {
+			System.out.println("------------------------------");
+			System.out.println("You are beginning your turn.");
+			// starting the round
 			playersHand.add(d.deal());
 			playersHand.add(d.deal());
 			System.out.println("Your hand: " + playersHand.get(0) + " and " + playersHand.get(1));
-			int sum = d.getValue(playersHand.get(0)) + d.getValue(playersHand.get(1));
-			System.out.println("Value: " + sum);
+			playerResult = d.getValue(playersHand.get(0)) + d.getValue(playersHand.get(1));
+			System.out.println("Value: " + playerResult);
 
-			if(sum > 21) {
+			computersHand.add(d.deal());
+			computersHand.add(d.deal());
+
+			System.out.println("Computers hand: " + computersHand.get(0));
+			computerResult = d.getValue(computersHand.get(0));
+			System.out.println("Value: " + computerResult);
+
+			if(playerResult > 21) {
 				System.out.println("You busted!");
-				playerTurn = false;
-				break;
 			}
-			else if( sum == 21 ) {
-				System.out.println("Blackjack! We have a winner!");
-				playerTurn = false;
-				// TODO: need to remember to update the user results
-				break;
+			else if(playerResult == 21) {
+				System.out.println("Blackjack!");
 			}
 			else {
 				do {
-					System.out.print("Would you like a hit? <y/n>");
-					choice = sc.next().charAt(0);
+					if(playerTurn) {
+						System.out.print("Would you like a hit? <y/n>");
+						choice = sc.next().charAt(0);
 
-					if(choice == 'y' && sum <= 21) {
-						playersHand.add(d.deal());
-						System.out.println("You drew " + playersHand.get(2));
-						sum += d.getValue(playersHand.get(2));
-						System.out.println("Value: " + sum);
+						if(choice == 'y') {
+							if(playerResult < 21)
+								playerResult += takeHit(playerTurn, playerResult);
+
+							if(playerResult > 21) {
+								System.out.println("You busted!");
+								break;
+							}
+						}
+						else {
+							System.out.println("Good choice. Staying with " + playerResult);
+							break;
+						}
 					}
-					else {
-						System.out.println("Good choice.");
-						playerTurn = false;
-						break;
-					}
-				}
-				while(choice == 'y'); // end of hit loop
+				} while(choice == 'y'); // end of hit loop
 			}
-		} while(playerTurn); // end of player turn loop
+
+			playerTurn = false;
+		}
+
+		System.out.println("Computer is revealing its hand.");
+		System.out.println("Computers hand: " + computersHand.get(0) + " and " + computersHand.get(1));
+		computerResult += d.getValue(computersHand.get(1));
+		System.out.println("Value: " + computerResult);
+
+		if(computerResult > 21) {
+			if(playerResult <= 21) {
+				System.out.println("Computer busted, congratulations, you won!");
+			}
+			else {
+				System.out.println("You both busted. It's a push.");
+			}
+		}
+		else if(computerResult == 21) {
+			if(playerResult > 21 || playerResult < 21) {
+				System.out.println("Computer got Blackjack! You lost.");
+			}
+			else {
+				System.out.println("Computer got Blackjack too! It's a push.");
+			}
+		}
+		else if(computerResult >= 17 && computerResult < 21) {
+			System.out.println("Computer will stay.");
+		}
+		else {
+			do {
+				computerResult += takeHit(playerTurn, computerResult);
+			} while(computerResult < 17);
+		}
+
+		playerTurn = true;
+
+		if(playerResult > computerResult) {
+			System.out.println("Congratulations! You won!");
+		}
+		else if(playerResult == computerResult) {
+			System.out.println("It's a push. You both had " + playerResult);
+		}
+		else {
+			System.out.println("You lost.");
+		}
+	}
+
+	public static int takeHit(boolean playerTurn, int sum) {
+		Card temp = d.deal();
+
+		if(playerTurn) {
+			System.out.println("You drew " + temp);
+			sum += d.getValue(temp);
+			System.out.println("Value: " + sum);
+		}
+		else {
+			System.out.println("Computer drew " + temp);
+			sum += d.getValue(temp);
+			System.out.println("Value: " + sum);
+		}
+
+		return sum;
 	}
 }
